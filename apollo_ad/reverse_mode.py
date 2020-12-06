75f15c0dc193ea948f8ed35207fdac97bcff8836
connorcapitolo <connorcapitolo@yahoo.com>
2020-12-05 17:14:28 -0800

    Issues with negation/subtraction for reverse mode

--- apollo_ad/reverse_mode.py
+++ apollo_ad/reverse_mode.py
@@ -42,116 +42,38 @@ class Reverse_Mode:
     def __rmul__(self, other):
         return self.__mul__(other)
 
-    def sin(self):
-        f = Reverse_Mode(np.sin(x.var))
-        self.child.append((np.cos(x.var), f))
-        return f
+# HAVING ISSUES WITH NEGATION/SUBTRACTION
+    # def __sub__(self, other):
+    #     return self.__add__(-other)
 
+    # def __rsub__(self, other):
+    #     return (-self).__add__(other)
 
-x = Reverse_Mode(0.5)
-y = Reverse_Mode(4.2)
-# does multiplication, then sin, then addition
-# f = x * y + Reverse_Mode.sin(x)
-
-# assert abs(f.var - 2.579425538604203) <= 1e-15
-# assert abs(x.grad() - (y.var + np.cos(x.var))) <= 1e-15
-# assert abs(y.grad() - x.var) <= 1e-15
-
-# f = 2 * x * y + Reverse_Mode.sin(x)
-
-# assert abs(f.var - (2 * 0.5 * 4.2 + np.sin(0.5))) <= 1e-15
-# assert abs(x.grad() - (2 * y.var + np.cos(x.var))) <= 1e-15
-# assert abs(y.grad() - 2 * x.var) <= 1e-15
-
-# f = 2 * x * y + 3 + Reverse_Mode.sin(x)
-
-# assert abs(f.var - (2 * 0.5 * 4.2 + np.sin(0.5) + 3)) <= 1e-15
-# assert abs(x.grad() - (2 * y.var + np.cos(x.var))) <= 1e-15
-# assert abs(y.grad() - 2 * x.var) <= 1e-15
-
-f = 4*x+Reverse_Mode.sin(x)+6*y
-
-assert np.round(f.var,4) == 27.6794 and np.round(x.grad(), 4) == 4.8776 and np.round(y.grad(), 4) == 6.0000
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-import numpy as np
-
-class Reverse_Mode:
-    # how should the seed be set for this?
-    def __init__(self, var, seed = 1):
-        self.var = var
-        self.child = []
-        self.der = seed
-
-    # this gets called on the way back
-    # how does it handle child?
-    def grad(self):
-        # base case for recursion: basically, we know that if node i has no child, then it's the output variable
-        if len(self.child) != 0:
-            # I'm using this based on the equation for lecture 12 we had in class
-            df_dui = 0
-            for duj_dui, df_duj in self.child:
-                df_dui += duj_dui * df_duj.grad()
-            self.der = df_dui
-        return self.der
-
-    def __add__(self, other):
-        try:
-            f = Reverse_Mode(self.var + other.var)
-            other.child.append((1.0, f))
-        except AttributeError:
-            f = Reverse_Mode(self.var + other)
-        self.child.append((1.0, f))
-        return f
-
-    def __radd__(self, other):
-        return self.__add__(other)
-
-    def __mul__(self, other):
-        try:
-            f = Reverse_Mode(self.var * other.var)
-            other.child.append((self.var, f)) # in this case, appending this to y
-        except AttributeError:
-            f = Reverse_Mode(self.var * other)
-        self.child.append((other, f))
-        return f
-
-    def __rmul__(self, other):
-        return self.__mul__(other)
+    # def __neg__(self):
+    #     # children = self.child
+    #     # f = Reverse_Mode(-self.var, -self.der)
+    #     # f.child = children
+    #     self.der = -self.der
+    #     self.var = -self.var
+    #     return self
 
 
     def sin(self):
-        f = Reverse_Mode(np.sin(x.var))
-        self.child.append((np.cos(x.var), f))
+        f = Reverse_Mode(np.sin(self.var))
+        self.child.append((np.cos(self.var), f))
         return f
 
+    def cos(self):
+        f = Reverse_Mode(np.cos(self.var))
+        self.child.append(-np.sin(self.var))
+        return f
 
-x = Reverse_Mode(0.5)
-y = Reverse_Mode(4.2)
-# does multiplication, then sin, then addition
-# f = x * y + Reverse_Mode.sin(x)
-# assert abs(f.var - 2.579425538604203) <= 1e-15
-# assert abs(x.grad() - (y.var + np.cos(x.var))) <= 1e-15
-# assert abs(y.grad() - x.var) <= 1e-15
+    
+# HAVING ISSUES WITH NEGATION/SUBTRACTION
+# x = Reverse_Mode(2)
+# y = Reverse_Mode(3)
 
-# f = 2 * x
-# print(x.grad()) #should return 2
-
-f = x * y + Reverse_Mode.sin(x)
-print(f.var)
-print(x.grad())
-print(y.grad())
-#assert abs(y.grad() - 2 * x.var) <= 1e-15
-'''
+# b = x-4
+# f = b-y
+# print(f.var)
+# print(y.grad())
--- apollo_ad/tests/reverse_mode_tests.py
+++ apollo_ad/tests/reverse_mode_tests.py
@@ -5,7 +5,7 @@ from ..reverse_mode import *
 
 class TestReverse:
 
-    def test_mult_and_add(self):
+    def test_multiple_inputs_one_output_and_add(self):
         x = Reverse_Mode(0.5)
         y = Reverse_Mode(4.2)
         f = 4*x+Reverse_Mode.sin(x)+6*y
@@ -20,3 +20,26 @@ class TestReverse:
         assert abs(f.var - (2 * 0.5 * 4.2 + np.sin(0.5))) <= 1e-15
         assert abs(x.grad() - (2 * y.var + np.cos(x.var))) <= 1e-15
         assert abs(y.grad() - 2 * x.var) <= 1e-15
+
+    def test_mult_add_three_inputs(self):
+        x = Reverse_Mode(1)
+        y = Reverse_Mode(2)
+        z = Reverse_Mode(3)
+        f = x + 3 * y + 2 * z
+        assert np.round(f.var, 4) == 13.0000
+        assert np.round(x.grad(), 4) == 1.0000
+        assert np.round(y.grad(), 4) == 3.0000
+        assert np.round(z.grad(), 4) == 2.0000
+
+# HAVING ISSUES WITH NEGATION/SUBTRACTION
+    # def test_subtraction(self):
+    #     x = Reverse_Mode(1)
+    #     f = x-4
+    #     assert np.round(f.var,4) == -3.0000
+    #     assert np.round(x.grad(),4) == 1.0000
+
+    #     y = Reverse_Mode(2)
+    #     f = x-4-y
+    #     assert np.round(f.var,4) == -5.0000
+    #     assert np.round(x.grad(),4) == 1.0000
+    #     assert np.round(y.grad(), 4) == -1.0000
