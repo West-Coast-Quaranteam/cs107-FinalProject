d5056ea35874bb5e80ee200a1abbad086e187f1d
Kexin Huang <kexinhuang@hsph.harvard.edu>
2020-12-07 13:56:38 -0800

    add UI fct

--- test.py
+++ test.py
@@ -19,3 +19,17 @@ print(z)
 # Value: [15.0100075   2.19523845] , 
 #	Der: [[-0.14112001  8.        ]
 #         [-0.09622504  0.5       ]]
+
+
+x = Variable(3, [1, 0, 0])
+y = Variable(4, [0, 1, 0])
+z = Variable(5, [0, 0, 1])
+f1 =  Variable.cos(x) + y ** 2 + Variable.sin(z)
+f2 = 2 * Variable.log(y) - Variable.sqrt(x)/3 
+z = Functions([f1, f2])
+
+print(z)
+# Value: [14.05108323  2.19523845] , 
+# Der: 
+# [[-0.14112001  8.          0.28366219]
+#  [-0.09622504  0.5         0.        ]]
