97d3638b02112347efc0a361f97657a85fb90145
connorcapitolo <connorcapitolo@yahoo.com>
2020-12-08 15:13:07 -0800

    Updating reverse mode

--- apollo_ad/reverse_mode.py
+++ apollo_ad/reverse_mode.py
@@ -1,21 +1,32 @@
 import numpy as np
+import cProfile
 
 class Reverse_Mode:
-    def __init__(self, var, seed = 1):
+    def __init__(self, var):
         self.var = var
         # need this because we need to store a form of the computational graph
         self.child = []
-        self.der = seed
+        self.der = None
 
-    def grad(self):
-        # base case for recursion: basically, we know that if node i has no child, then it's the output variable
-        if len(self.child) != 0:
+    def gradient(self):
+        # when calling this on the first variable, it should be that everything is self.der = None except for the output function, which has been initialized to 1
+        # if self.der is not None, then the derivative has already been calculated for that variable, and we should just return it rather than calculating everything again
+
+        if self.der is None:
             # I'm using this based on the equation for lecture 12 we had in class
             df_dui = 0
             for duj_dui, df_duj in self.child:
-                df_dui += duj_dui * df_duj.grad()
+                # I need this += so that I can handle when the node has more than one child
+                df_dui += duj_dui * df_duj.gradient()
             self.der = df_dui
         return self.der
+
+    def derivative(self, inputs, seed = 1):
+        self.der = seed
+        value = self.var
+        derivatives = np.array([i.gradient() for i in inputs])
+        return value, derivatives
+
 
     def __add__(self, other):
         try:
@@ -57,7 +68,6 @@ class Reverse_Mode:
     #     self.var = -self.var
     #     return self
 
-
     def sin(self):
         f = Reverse_Mode(np.sin(self.var))
         self.child.append((np.cos(self.var), f))
@@ -65,10 +75,39 @@ class Reverse_Mode:
 
     def cos(self):
         f = Reverse_Mode(np.cos(self.var))
-        self.child.append(-np.sin(self.var))
+        self.child.append((-np.sin(self.var), f))
         return f
 
-    
+    def exp(self):
+        f = Reverse_Mode(np.exp(self.var))
+        self.child.append((np.exp(self.var), f))
+        return f
+
+
+    def __repr__(self):
+        return 'Value: ' + str(self.var) + ' , Der: ' + str(self.der)
+
+    def __str__(self):
+        return 'Value: ' + str(self.var) + ' , Der: ' + str(self.der)
+
+# x = Reverse_Mode(1)
+# y = Reverse_Mode(2)
+# inputs = [x, y]
+# f1 = x*y+Reverse_Mode.exp(x*y)
+# f2 = x + 3 * y
+# value, check = f.derivative(inputs)
+
+# x = Reverse_Mode(1)
+# y = Reverse_Mode(2)
+# ind_vars = [x, y]
+# f = x*y+Reverse_Mode.exp(x*y)
+# # for i in ind_vars:
+# #     print(i.grad())
+# check = f.derivative([x, y])
+# print(check[0])  # [16.7781122  8.3890561]
+# #cProfile.run('f.derivative([x,y])')
+
+
 # HAVING ISSUES WITH NEGATION/SUBTRACTION
 # x = Reverse_Mode(2)
 # y = Reverse_Mode(3)
@@ -77,3 +116,35 @@ class Reverse_Mode:
 # f = b-y
 # print(f.var)
 # print(y.grad())
+
+
+'''
+MY CONCLUSION IS THAT IF YOU DON'T HAVE A CHILD NODE, THEN YOU'RE THE FUNCTION
+BASICALLY JUST NEED TO GET THE WEIGHT FOR THE SPECIFIC OPERATION, AND THEN CALL RECURSION UNTIL I GET TO REVERSE_MODE INSTANCE THAT HAS NO CHILDREN
+
+    EXAMPLES
+    =========
+    # multiple functions
+    >> x = Variable(3, [1, 0])
+    >> y = Variable(4, [0, 1])
+    >> f1 =  Variable.cos(x) + y ** 2
+    >> f2 = 2 * Variable.log(y) - Variable.sqrt(x)/3 
+    >> z = Functions([f1, f2])
+    Values: [XXX, XXX]
+    Derivative: 
+    [ [ xxx, xxx],
+    [ xxx, xxx] ]
+
+
+With forward mode, you only have to call everything once and don't have to store anything
+With reverse mode, need to store the input variables
+
+x = Reverse_Mode(1)
+y = Reverse_Mode(2)
+f = x * y + Variable.exp(x*y)
+f.derivative(seed = 1)
+
+
+I'm currently recomputing things in this
+
+'''
--- apollo_ad/tests/reverse_mode_tests.py
+++ apollo_ad/tests/reverse_mode_tests.py
@@ -5,31 +5,27 @@ from ..reverse_mode import *
 
 class TestReverse:
 
-    def test_multiple_inputs_one_output_and_add(self):
-        x = Reverse_Mode(0.5)
-        y = Reverse_Mode(4.2)
-        f = 4*x+Reverse_Mode.sin(x)+6*y
-        assert np.round(f.var, 4) == 27.6794
-        assert np.round(x.grad(), 4) == 4.8776
-        assert np.round(y.grad(), 4) == 6.0000
-
-    def test_mult(self):
-        x = Reverse_Mode(0.5)
-        y = Reverse_Mode(4.2)
-        f = 2 * x * y + Reverse_Mode.sin(x)
-        assert abs(f.var - (2 * 0.5 * 4.2 + np.sin(0.5))) <= 1e-15
-        assert abs(x.grad() - (2 * y.var + np.cos(x.var))) <= 1e-15
-        assert abs(y.grad() - 2 * x.var) <= 1e-15
+    def test_autoed_reverse_mode(self):
+        x = Reverse_Mode(1)
+        y = Reverse_Mode(2)
+        inputs = [x, y]
+        f = x*y+Reverse_Mode.exp(x*y)
+        value, check = f.derivative(inputs)
+        assert np.round(value, 4) == 9.3891
+        assert np.round(check[0],4) == 16.7781 and np.round(check[1], 4) == 8.3891
 
     def test_mult_add_three_inputs(self):
         x = Reverse_Mode(1)
         y = Reverse_Mode(2)
         z = Reverse_Mode(3)
         f = x + 3 * y + 2 * z
-        assert np.round(f.var, 4) == 13.0000
-        assert np.round(x.grad(), 4) == 1.0000
-        assert np.round(y.grad(), 4) == 3.0000
-        assert np.round(z.grad(), 4) == 2.0000
+        inputs = [x,y,z]
+        value, check = f.derivative(inputs)
+        assert np.round(value, 4) == 13.0000
+        assert np.round(check[0], 4) == 1.0000
+        assert np.round(check[1], 4) == 3.0000
+        assert np.round(check[2], 4) == 2.0000
+
 
 # HAVING ISSUES WITH NEGATION/SUBTRACTION
     # def test_subtraction(self):
