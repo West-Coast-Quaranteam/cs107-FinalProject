0b3768f960bfe30030aea13b875563cbecff01da
connorcapitolo <connorcapitolo@yahoo.com>
2020-12-11 19:02:16 -0800

    Updating to version 0.0.7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(this_directory, 'requirements.txt'),
 setup(
   name = 'apollo_ad',
   packages = ['apollo_ad'], 
-  version = '0.0.6',
+  version = '0.0.7',
   license='MIT',
   description = 'Auto Differentiation Tools',
   long_description=readme(),
